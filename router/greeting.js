a205d90b841cd158c8dbf56be7bfb9c10f4e7a42
Cody Chen <cdxvy30@gmail.com>
2023-12-15 02:35:34 +0800

    update: swagger docs

--- router/greeting.js
+++ router/greeting.js
@@ -4,7 +4,7 @@ const controller = require('../controller/greeting');
 
 router.get('/birthday',
   /*  #swagger.tags = ['Greetings']
-      #swagger.description = 'Get birthday greetings for users whose birthdays are on the current day. Returns an array of birthday greetings, or an empty array if no users have a birthday. Different gender will get different discount items.'
+      #swagger.description = 'Get birthday greetings for users whose birthdays are on the current day. Returns an array of birthday greetings, or an empty array if no users have a birthday. Elder(>49) will get an additional greting picture!'
       #swagger.responses[200] = {
           description: 'Success',
           schema: {
@@ -22,6 +22,10 @@ router.get('/birthday',
                               content: {
                                   type: 'string',
                                   example: "Happy birthday, dear [Name]! We offer special discount for the following items: [Items]"
+                              },
+                              image: {
+                                type: 'string',
+                                example: "data:image/png;base64,[base64Image]"
                               }
                           }
                       }
