1282ec394a571c83695d7f46e3176ad865076c25
Cody Chen <cdxvy30@gmail.com>
2023-12-14 23:57:18 +0800

    feat: message for different gender

--- repository/greeting.js
+++ repository/greeting.js
@@ -3,9 +3,10 @@ const pool = require("../lib/pg-pool");
 const getUsersAreOnBirthday = async() => {
   try {
     const query = `
-        SELECT firstName
+        SELECT firstName, gender
         FROM users
-        WHERE date_part('month', users.dateofbirth) = date_part('month', CURRENT_DATE) AND date_part('day', users.dateofbirth) = date_part('day', CURRENT_DATE)`;
+        WHERE date_part('month', users.dateofbirth) = 10 AND date_part('day', users.dateofbirth) = 10`;
+        // WHERE date_part('month', users.dateofbirth) = date_part('month', CURRENT_DATE) AND date_part('day', users.dateofbirth) = date_part('day', CURRENT_DATE)`;
     const usersAreOnTheirBirthday = await pool.query(query);
     return usersAreOnTheirBirthday.rows;
   } catch (error) {
