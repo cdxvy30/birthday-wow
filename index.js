bdcb9ac61ec1b8c5bbb946168d32b1ed178313c2
Cody Chen <cdxvy30@gmail.com>
2023-12-14 23:18:46 +0800

    feat: add swagger docs

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const express = require('express');
 const app = express();
 const port = 5050;
 const greetingRouter = require('./router/greeting');
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./docs/swagger.json");
 
 app.use("/api/v1/greeting", greetingRouter);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(port, () => {
   console.log(`Server listening on PORT ${port}`);
--- router/greeting.js
+++ router/greeting.js
@@ -2,6 +2,21 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controller/greeting');
 
-router.get('/birthday', controller.birthdayGreeting);
+router.get('/birthday',
+  /* 	#swagger.tags = ['Greetings']
+      #swagger.description = 'A list of birthday greetings for users on birthday on the current day, or an empty array if no birthdays are found'
+      #swagger.responses[200] = {
+          description: 'Success',
+          schema: {
+              data: [
+                  {
+                      "title": "Subject: Happy Birthday!",
+                      "content": "Happy birthday, dear Robert!"
+                  }
+              ]
+          }
+      }
+  */
+  controller.birthdayGreeting);
 
 module.exports = router;
