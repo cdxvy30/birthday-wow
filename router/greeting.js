500063e584567c0545a0fa3dbaa07a262f25c5c9
Cody Chen <cdxvy30@gmail.com>
2023-12-15 07:23:37 +0800

    fix: update api docs

--- router/greeting.js
+++ router/greeting.js
@@ -4,29 +4,16 @@ const controller = require('../controller/greeting');
 
 router.get('/birthday',
   /*  #swagger.tags = ['Greetings']
-      #swagger.description = 'Get birthday greetings for users whose birthdays are on the current day. Returns an array of birthday greetings, or an empty array if no users have a birthday.'
+      #swagger.description = 'Get birthday greetings for users whose birthdays are on the current day. The response is in XML format and contains greetings within a root element.'
       #swagger.responses[200] = {
-          description: 'Success',
-          schema: {
-              type: 'object',
-              properties: {
-                  data: {
-                      type: 'array',
-                      items: {
-                          type: 'object',
-                          properties: {
-                              title: {
-                                  type: 'string',
-                                  example: "Subject: Happy Birthday!"
-                              },
-                              content: {
-                                  type: 'string',
-                                  example: "Happy birthday, dear [firstName]!"
-                              },
-                          }
-                      }
-                  }
+          description: 'An XML list of birthday greetings for users, or an empty root element if no users have a birthday.',
+          content: {
+            'application/xml': {
+              schema: {
+                type: 'string',
+                example: '<root><greeting><title>Subject: Happy Birthday!</title><content>Happy birthday, dear John!</content></greeting><greeting><title>Subject: Happy Birthday!</title><content>Happy birthday, dear Jane!</content></greeting></root>'
               }
+            }
           }
       }
   */
