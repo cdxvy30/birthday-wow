8db437a7ced89b5e93cd5e0470bd6066b7bf1a1d
Cody Chen <cdxvy30@gmail.com>
2023-12-15 00:40:25 +0800

    v2

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const greetingRouter = require('./router/greeting');
 const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("./docs/swagger.json");
 
-app.use("/api/v1/greeting", greetingRouter);
+app.use("/api/v2/greeting", greetingRouter);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(port, () => {
