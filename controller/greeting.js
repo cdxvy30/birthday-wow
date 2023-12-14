44ccba09c41ba7e0b4222964195679d1d1f85159
Cody Chen <cdxvy30@gmail.com>
2023-12-15 07:21:10 +0800

    feat: json to xml

--- controller/greeting.js
+++ controller/greeting.js
@@ -1,14 +1,24 @@
+const xml = require('xml');
 const repository = require('../repository/greeting');
 const { genGreetingMessage } = require("../lib/msgGenerator");
 
 const birthdayGreeting = async(req, res) => {
   try {
     const birthdayUsers = await repository.getUsersAreOnBirthday();
-    const data = []
-    for (const user of birthdayUsers) {
-      data.push(genGreetingMessage(user.firstName));
-    }
-    return res.status(200).send({data});
+    const greetings = birthdayUsers.map(user => {
+      const greetingMessage = genGreetingMessage(user.firstName);
+      return {
+        greeting: [
+          { title: greetingMessage.title },
+          { content: greetingMessage.content }
+        ]
+      };
+    });
+
+    const xmlResponse = xml({ root: greetings });
+
+    res.setHeader('Content-Type', 'application/xml');
+    return res.status(200).send(xmlResponse);
   } catch (error) {
     console.log(error);
     return res.status(500).send("Server Error.");
